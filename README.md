cf64d50bee670a3ba833925c21c84162d9cda2c7
Angel Diaz <miguelangeldiazmejia79@gmail.com>
2024-10-25 22:48:52 -0600

    Agregar archivos de proyecto.

---
+++ README.md
@@ -0,0 +1,1 @@
+Tarea 1.2 Aplicación de Autores
---
+++ tarea1.2/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace tarea1._2
+{
+    public partial class App : Application
+    {
+        public static Controllers.AutorController AutorController { get; private set; }
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new vistas.MainPage());
+            AutorController = new Controllers.AutorController();
+        }
+    }
+}
+
